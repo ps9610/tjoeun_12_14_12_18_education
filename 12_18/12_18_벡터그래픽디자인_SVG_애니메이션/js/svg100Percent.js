03a826db93d73199f3bb3ceb1f339486e41f80a3
ps9610 <ps961001@gmail.com>
2020-12-18 17:45:42 +0900

    백분율카운트_산출물_수업자료

--- 12_18/12_18_벡터그래픽디자인_SVG_애니메이션/js/svg100Percent.js
+++ 12_18/12_18_벡터그래픽디자인_SVG_애니메이션/js/svg100Percent.js
@@ -1,29 +1,50 @@
 ;(function(window,document,$,undefined){
-    //SVG Animation
-    var circleSvg = $(".front circle");//원형(circle) 객체를 변수로 놓음
-    var totLength = null; 
+    //SVG Animation 백분율 카운터
+    var svgCircle = $(".front").find("circle");
+    var percent  = 1 //%
+    var totLen = 0;
+    var tot = 0;
+    var piece = 0;
+    var secondTime = 3;
+    var setId = 0;
+    var x = 0; 
+        
+    svgCounterFn();
+    function svgCounterFn(){
+        $.each(svgCircle, function(idx,obj){
+            totLen = obj.getTotalLength();
 
-    circleSvgFn();
-    setInterval(circleSvgFn, 6000);
-    
-    function circleSvgFn(){ //조각 조각이 몇개가 만나야 애니메이션이 되는가
-        $.each(circleSvg, function(idx, obj){ //circleSvg를 읽어들임
-            totLength = obj.getTotalLength(); //객체의 전체 총(Total) 길이(Length)를 가져오라.는 변수를 만들어줌
-            //totLength*(1-1) // 0%만 남겨놓고 다 채운다. = 전부 다 채운다.
-            //totLength*(1-0.8) // 20%만 남겨놓고 다 채운다.
-            //console.log(totLength);
+                //객체 스타일 배열(Dasharray)과 offset(Dashoffset)
+                obj.style.strokeDasharray  = totLen;
+                obj.style.strokeDashoffset = totLen;
+            //응용 타이머
+            //5초동안 96%를 채우는 원그래프를 프로그래밍하는 알고리즘 구현
+            console.log(totLen); //총길이 1545.0391845703125를5로나눔/=309.0078369/👈1초동안이동하는길이
+            //piece = totLen/secondTime/1; //5초로 나눈//=30.90078369/1초 당 조각 나오게 계산//단위 : 1000밀리초
+            //piece = totLen/secondTime/10; //5초로 나눈//=3.090078369/1초 당 조각 나오게 계산//단위 : 100밀리초
+            piece = totLen/secondTime/100; //5초로 나눈//=0.3090078369/1초 당 조각 나오게 계산//단위 : 10밀리초
+            //piece = totLen/secondTime/1000; //5초로 나눈//=0.03090078369/1초 당 조각 나오게 계산//단위 : 1밀리초
+                // 1초동안이동하는길이 309.0078369
+                // 5초 동안에 이동하는 길이를 모두 더해서
+                // 총길이를 초과하면 끝나는 반복 알고리즘
+            
+            setId = setInterval(function(){
+                tot += piece;
 
-            //객체 스타일 전체 길이로 지정
-            obj.style.strokeDasharray = totLength; //getTotalLength로 Dash~의 공간을 채워라
-            obj.style.strokeDashoffset = totLength; //getTotalLength로 Dash~의 공간을 채워라
-
-            $(obj).animate({strokeDasharray:0, strokeDashoffset:0},3000,function(){ // 채우고 다 채우면
-                $(obj).animate({strokeDasharray:totLength, strokeDashoffset:totLength},1000) // 지워라
-            });
-
+                if( tot > Math.round(totLen*percent) ){ //누적 piece 합계가 전체길이 totLen 초과하면 끝
+                    clearInterval(setId);}
+                    
+                    //원 그래프
+                        $(obj).css({strokeDashoffset: totLen-tot });
+                        /* 총길이-누적합/전체길이에서줄어들면서 */ 
+                        //주어진 숫자대로 진행되게끔 만들어주었으니까 animate할 필요 없음
+                    
+                        //숫자 카운트하면됨
+                    x = Math.round((tot/totLen)*100) + "%"; //현재 숫자를 전체 숫자로 나눈다음 곱하기 백
+                    $(".num h2").html( x /* 백분율 = tot(총합계)/totLen */ );
+            },1000/100) //여기서 숫자를 쪼개기 때문에 math.round가 필요함
 
         });
     }
 
 })(window,document,jQuery);
-    
